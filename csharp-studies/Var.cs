5e7da7b51474ed499c515809437ecf8af9c7144e
Thaynara Dutra <thaynaracdutra@outlook.com>
2025-03-14 22:17:47 -0300

    add variables and constants studies

---
+++ csharp-studies/Var.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.VisualBasic;
+
+namespace csharp_studies;
+
+public class Var
+{
+    public static void Main()
+    {
+        string myName = "Thaynara Dutra";
+        int number = 10;
+        number = 1;
+        myName = myName + number;
+        char character = 'a';
+        bool boolean = false;
+
+
+        Console.WriteLine(myName);
+
+        Console.WriteLine(number);
+
+        Console.WriteLine(character);
+
+        Console.WriteLine(boolean);
+    }
+}
+
+
+
