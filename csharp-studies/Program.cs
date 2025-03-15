e991e13744cfceb837a8d3ec8a1a3c4834ea13bb
Thaynara Dutra <thaynaracdutra@outlook.com>
2025-03-14 21:49:12 -0300

    Adicionar arquivos de projeto.

---
+++ csharp-studies/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.VisualBasic;
+
+namespace csharp_studies;
+
+public class Program
+{
+    public static void Main()
+        {
+        string myName = "Thaynara";
+
+        Console.WriteLine(myName); 
+    }
+}
+
+
+
